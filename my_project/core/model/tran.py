9b8c01df24b9a76d9b84b5721dd50877bf7a934f
harmless95 <213446016+harmless95@users.noreply.github.com>
2026-01-28 23:36:27 +0700

    I uploaded a model for translating text from English to Russian.

--- my_project/core/model/tran.py
+++ my_project/core/model/tran.py
@@ -1,16 +1,20 @@
 from transformers import pipeline
-
 from core.config import setting
 
 model_translate = setting.conf_stt.path_model_translate
+model_translate_ru = setting.conf_stt.path_model_translate_ru
 
-# # берем из кеша
-# translator = pipeline("translation", model="Helsinki-NLP/opus-mt-ru-en", device=0)
 
 # для docker там уже скачано и мы берем из кеша
 translator = pipeline("translation", model=model_translate, device=0)
+translator_ru = pipeline("translation", model=model_translate_ru, device=0)
 
 
 def translate_text(text_ru: str):
     result = translator(text_ru)
+    return result
+
+
+def translate_text_ru(text_ru: str):
+    result = translator_ru(text_ru)
     return result
